6e8522b3a1c0dcbf662afe93fa40fb789079073c
Hélène CARRIOU <helenec56@laposte.net>
2022-01-31 19:53:16 +0100

    corps texte class Article

---
+++ old-articles/article-iode.php
@@ -0,0 +1,32 @@
+<?php include('header.php'); ?>
+
+<main id="contenu">
+
+    <section id="lesposts" class="full container">
+        <div class="retour">
+            <a href="actualite.php"><i class="fas fa-arrow-circle-left"></i>  Retour</a>
+        </div>
+        <article class="post">
+            <figure class="imgsize">
+                <img src="images/post-iode.jpg" alt="Recherches sur l'usine d'iode">
+            </figure>
+            <div class="text-post">
+                <h2>Recherches sur l'usine d'iode de Kerpen</h2>
+                <time datetime="2021-02-06">06 février 2021</time>
+                <p>Voici la restitution de nos recherches les plus récentes. Elles mettent en avant l'usine d'iode de Kerpen.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero rerum, praesentium dolorem ipsum accusantium dolorum cupiditate? Molestias fugit reiciendis possimus cumque corrupti obcaecati, sit molestiae, itaque consequatur officia pariatur beatae.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste quam voluptatibus praesentium quibusdam perferendis, consequuntur necessitatibus fugiat eum temporibus delectus.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nihil asperiores magni nemo harum velit hic deleniti obcaecati. Eos, accusantium.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ex vitae odio. Maxime et, nobis quibusdam earum dolor, commodi repudiandae ea inventore ab laudantium totam nesciunt accusamus debitis reiciendis? Doloremque minima quibusdam dolorum facilis aspernatur recusandae cupiditate dolorem totam at?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut explicabo consequuntur reiciendis quia, ipsam cum ab. Excepturi, deleniti necessitatibus? Nam eius, maxime cupiditate repellendus impedit pariatur reiciendis. Explicabo, esse repellat quas vero qui quidem delectus quam expedita ab hic nemo adipisci? Voluptatum accusantium tempora nam!</p>
+            </div>
+        </article>
+        <div class="retour">
+            <a href="actualite.php"><i class="fas fa-arrow-circle-left"></i>  Retour</a>
+        </div>
+    </section>
+    
+</main>
+
+<?php include('footer.php'); ?>
